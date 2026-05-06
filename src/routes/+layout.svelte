f4a7a3e4da8f8b644c98d6eff9dd49df19dfea34
Arufaian <alfianilyaysa13@gmail.com>
2026-05-06 15:25:05 +0700

    feat: setup mode watcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import './layout.css';
+	import { ModeWatcher } from 'mode-watcher';
 	import favicon from '$lib/assets/favicon.png';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 
@@ -8,5 +9,6 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
+<ModeWatcher />
 <Toaster richColors />
 {@render children()}
