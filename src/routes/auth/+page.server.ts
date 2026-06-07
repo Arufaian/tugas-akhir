a8c125b9c45dbc0a3723921da8140e962d4444c8
Arufaian <alfianilyaysa13@gmail.com>
2026-06-07 19:22:29 +0700

    Revert "feat: add redirect when access /auth"
    This reverts commit f122fa9a231887234dc03e803f0db8523cf3f4b4.

--- src/routes/auth/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import type { PageServerLoad } from '../admin/$types';
-import { redirect } from '@sveltejs/kit';
-
-export const load: PageServerLoad = async () => {
-	throw redirect(302, '/auth/login');
-};
