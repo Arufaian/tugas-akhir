49d5bbb2e38faa5dd41941190d10637032a84234
Arufian <alfianilyasya13@gmail.com>
2026-06-17 13:18:03 +0700

    chore: update data-table action for table alternative

--- src/routes/admin/alternatives/data-table-actions.svelte
+++ src/routes/admin/alternatives/data-table-actions.svelte
@@ -1,31 +1,33 @@
 <script lang="ts">
 	import EllipsisIcon from '@lucide/svelte/icons/ellipsis';
+	import PencilIcon from '@lucide/svelte/icons/pencil';
+	import Trash2Icon from '@lucide/svelte/icons/trash-2';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 
-	let { id }: { id: string } = $props();
+	// let { id }: { id: string } = $props();
 </script>
 
 <div class="flex items-center justify-center">
 	<DropdownMenu.Root>
 		<DropdownMenu.Trigger>
 			{#snippet child({ props })}
-				<Button {...props} variant="ghost" size="icon" class="relative size-8 p-0">
+				<Button {...props} variant="ghost" size="icon" class="relative  size-8 p-0">
 					<span class="sr-only">Open menu</span>
 					<EllipsisIcon />
 				</Button>
 			{/snippet}
 		</DropdownMenu.Trigger>
-		<DropdownMenu.Content>
-			<DropdownMenu.Group>
-				<DropdownMenu.Label>Actions</DropdownMenu.Label>
-				<DropdownMenu.Item onclick={() => navigator.clipboard.writeText(id)}>
-					Copy payment ID
-				</DropdownMenu.Item>
-			</DropdownMenu.Group>
+		<DropdownMenu.Content class="mr-4">
+			<DropdownMenu.Item disabled>
+				<PencilIcon />
+				Edit
+			</DropdownMenu.Item>
 			<DropdownMenu.Separator />
-			<DropdownMenu.Item>View customer</DropdownMenu.Item>
-			<DropdownMenu.Item>View payment details</DropdownMenu.Item>
+			<DropdownMenu.Item variant="destructive">
+				<Trash2Icon />
+				Delete
+			</DropdownMenu.Item>
 		</DropdownMenu.Content>
 	</DropdownMenu.Root>
 </div>
